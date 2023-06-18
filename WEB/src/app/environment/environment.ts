61de4f743bec35501c5256b26ce3b4a14675e541
Adrian Ibañez Montalvo <adry.im98@gmail.com>
2023-06-18 15:20:26 +0200

    Commit final

--- WEB/src/app/environment/environment.ts
+++ WEB/src/app/environment/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  // API_URL: "http://localhost:8001/api/"
-  API_URL: "https://e8e2-37-14-251-10.ngrok-free.app/api/"
+  API_URL: "http://localhost:8001/api/"
+  // API_URL: "https://e8e2-37-14-251-10.ngrok-free.app/api/"
 };
 
